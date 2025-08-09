a37e5ecb9af34fcdd62acead82b9640c9b2f0a5b
Shalomi <shalomi01.perera@gmail.com>
2025-08-09 12:02:44 +0530

    project initialized with user login and product manaaging features

---
+++ controllers/userController.js
@@ -0,0 +1,93 @@
+import User from "../Models/user.js";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
+
+export function createUser(req, res){
+   
+   const hashedPassword = bcrypt.hashSync(req.body.password,10)
+
+    const user = new User(
+         {
+            email : req. body. email,
+            fristName : req. body. fristName,
+            lastName : req. body. lastName,
+            password : hashedPassword
+         }
+    )
+      user.save().then(
+            ()=>{
+                res.json(
+                    {
+                       message:"User created successfully" 
+                    })
+            }
+        ).catch(
+            ()=>{
+                res.json(
+                    {
+                        message:"Falied to create User"
+                    })
+                }
+        )
+    }
+
+export function loginUser(req, res){
+  User.findOne(
+        {
+            email : req.body.email
+        }
+    ).then(
+        (user)=>{
+            if(user == null){
+               res.status(407).json(
+                {
+                    message : "User not Found"
+                }
+               ) 
+
+            }else{
+                const isPasswordMaching = bcrypt.compareSync(req.body.password, user.password)
+                if (isPasswordMaching){
+
+                    const token = jwt.sign(
+                        {
+                            email: user.email,
+                            fristName: user.fristName,
+                            LastName: user.lastName,
+                            role: user.role,
+                            isEmailVerified: user.isEmailVerified,
+                        },
+                        "jwt secret"
+                    )
+                    
+                    res.json(
+                        {
+                            message : "Login Successful",
+                            token: token
+                        }
+                    )
+
+
+                }else{
+                    res.status(401).json(
+                        {
+                            message : "Invailed Password"
+                        }
+                    )
+                }
+            }
+        }
+    )
+}
+
+export function isadmin(req){
+    
+    if (req.user == null){
+        return false;
+    }
+    if(req.user.role != "admin"){
+        return false;
+    }
+
+    return true;
+}
---
+++ index.js
@@ -0,0 +1,53 @@
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./routes/userRouter.js";
+import jwt from "jsonwebtoken";
+import productRouter from "./routes/productRouter.js";
+
+const app = express()
+
+app.use(express.json())
+
+app.use(
+    (req,res,next)=>{
+
+        let token = req.header("Authorization")
+        
+        if(token != null){
+            token = token.replace("Bearer " ,"")
+            jwt.verify(token,"jwt.secret",
+                (err, decoded)=>{
+                    if(decoded == null){
+                        res.json({
+                            message:"Invalied token please Login again"
+                        })
+                        return
+                    }else{
+                        req.user = decoded
+                    }
+                
+            })
+        }
+        next()
+    }
+)
+
+const connectionstring = "mongodb+srv://admin:456@cluster0.rnn5bii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+mongoose.connect (connectionstring).then(
+    ()=>{
+        console.log("Database connected")
+    }
+).catch(
+    ()=>{
+        console.log("Database connection failed")
+    }
+)   
+
+app.use("/user", userRouter)
+app.use("/products", productRouter)
+
+app.listen(5000, 
+    ()=>{
+        console.log("Server is runing on port 5000")
+    }
+)
